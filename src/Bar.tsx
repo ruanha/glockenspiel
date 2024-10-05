590267a92b56ded4b0e31f8d66907cc7ae589e73
Rune Hartvig <rune_hartvig@hotmail.com>
2024-10-05 20:31:25 +0200

    initial style of the bars

--- src/Bar.tsx
+++ src/Bar.tsx
@@ -1,5 +1,11 @@
-function Bar({ note }: { note: string }) {
-  return <div>{note}</div>;
+function Bar({ note, color }: { note: string; color: string }) {
+  return (
+    <div
+      className={`w-14 h-28 flex place-items-center justify-center rounded ${color}`}
+    >
+      {note}
+    </div>
+  );
 }
 
 export default Bar;
--- src/Xylophone.tsx
+++ src/Xylophone.tsx
@@ -1,13 +1,22 @@
 import Bar from "./Bar";
 
 function Xylophone() {
-  // ten notes of the xylophone
-  const notes = ["C", "D", "E", "F", "G", "A", "B", "C", "D", "E"];
+  const bars = [
+    { note: "C", color: "bg-red-500" },
+    { note: "D", color: "bg-orange-500" },
+    { note: "E", color: "bg-yellow-500" },
+    { note: "F", color: "bg-green-500" },
+    { note: "G", color: "bg-blue-500" },
+    { note: "A", color: "bg-indigo-500" },
+    { note: "B", color: "bg-purple-500" },
+    { note: "C", color: "bg-red-200" },
+    { note: "D", color: "bg-orange-200" },
+    { note: "E", color: "bg-yellow-200" },
+  ];
   return (
-    <div>
-      <h1>Xylophone</h1>
-      {notes.map((note, index) => (
-        <Bar key={index} note={note} />
+    <div className="flex flex-row gap-8">
+      {bars.map((bar, index) => (
+        <Bar key={index} note={bar.note} color={bar.color} />
       ))}
     </div>
   );
