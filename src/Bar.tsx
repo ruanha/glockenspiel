4be6f5347c3c813da94cc679e8089b94e7aebd77
Rune Hartvig <rune_hartvig@hotmail.com>
2024-10-13 20:53:30 +0200

    no select of the text on the bars

--- src/Bar.tsx
+++ src/Bar.tsx
@@ -5,7 +5,20 @@ function Bar({ note, color }: { note: string; color: string }) {
   };
   return (
     <div
-      className={`w-12 h-40 md:w-16 md:h-64 lg:w-20 lg:h-72 flex place-items-center justify-center rounded-lg ${color}`}
+      className={`
+        w-12
+        h-40
+        md:w-16 
+        md:h-64
+        lg:w-20
+        lg:h-72
+        flex
+        place-items-center
+        justify-center
+        rounded-lg 
+        ${color}
+        select-none
+        `}
       onClick={() => playNote()}
     >
       {note}
